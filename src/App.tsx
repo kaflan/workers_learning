0cd278ed9b5e23844c006344a869331b7c203636
andreykatrusha <andrey.katrusha@gmail.com>
2019-12-10 19:45:21 +0200

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
+const App: FC = () => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +19,6 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
-}
+  )
 
 export default App;
